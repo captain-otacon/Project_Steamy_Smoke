bb6b46b538eeca52b1bc7e12ee9d0cbb823832f3
captain-otacon <eero.taponen@aol.com>
2016-01-05 13:29:50 +0200

    no message

---
+++ Project_Steamy_Smoke/Assets/Scripts/CharacterCreation/PlayerBackGround.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerBackGround : MonoBehaviour {
+	int valuetext;
+	Text raceMenu;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaceBackground ();
+	}
+
+	void RaceBackground () {
+		if (valuetext == 0) {
+			raceMenu.text = "Human Race";
+		} else if (valuetext == 1) {
+			raceMenu.text = "Elf Race";
+		} else if (valuetext == 2) {
+			raceMenu.text = "Dwarf Race";
+		}
+	}	
+}
