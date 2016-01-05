97e702223d06b8a17ac13d87ee27d5716ede966d
captain-otacon <eero.taponen@aol.com>
2016-01-06 00:33:03 +0200

    Vihdoinkin sain korjattua raceTekstin

--- Project_Steamy_Smoke/Assets/Scripts/CharacterCreation/PlayerBackGround.cs
+++ Project_Steamy_Smoke/Assets/Scripts/CharacterCreation/PlayerBackGround.cs
@@ -4,10 +4,10 @@ using UnityEngine.UI;
 
 public class PlayerBackGround : MonoBehaviour {
 	public Dropdown raceMenu;
-	public Text raceText;
+	public Text text;
 	// Use this for initialization
 	void Start () {
-		
+		text = gameObject.GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
@@ -17,11 +17,11 @@ public class PlayerBackGround : MonoBehaviour {
 
 	public void UpdateText () {
 		if (raceMenu.value == 0) {
-			raceText.text = "Human";
+			text.text = "Human's are most common type of creature in the world of Galerracia and you can find then in any kind of profession. Best race for multiclassing. +1 for every ability";
 		} else if (raceMenu.value == 1) {
-			raceText.text = "Elf";
+			text.text = "Elf";
 		} else if (raceMenu.value == 2) {
-			raceText.text = " Dwarf";
+			text.text = " Dwarf";
 		}
 	}
 }
