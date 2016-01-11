84e76c8d2c87e4e5d657d3d872f30bddf1beef1e
captain-otacon <eero.taponen@aol.com>
2016-01-11 12:54:59 +0200

    Abilitypoint systeemi valmis

--- Project_Steamy_Smoke/Assets/Scripts/Character/AbilityPoints.cs
+++ Project_Steamy_Smoke/Assets/Scripts/Character/AbilityPoints.cs
@@ -1,17 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class AbilityPoints : MonoBehaviour {
-	public enum Race {Human, Elf, Dwarf};
+	public int Str = 8;
+	public int Dex = 8;
+	public int Con = 8;
+	public int Int = 8;
+	public int Wis = 8;
+	public int Cha = 8;
+
+	public int Race; //0 on ihminen, 1 on elffi, 2 on kääpiö
+	Dropdown RaceDropdown;
 	// Use this for initialization
-	void Start () {
-		Race value = Race.Human;
-		if (value == Race.Human) {
+	void Awake () {
+		RaceDropdown = GameObject.Find ("PlayerRace").GetComponent<Dropdown> ();
+		Race = RaceDropdown.value;
+		if (Race == 0) {
+			Str = Str + 1;
+			Dex = Dex + 1;
+			Con = Con + 1;
+			Int = Int + 1;
+			Wis = Wis + 1;
+			Cha = Cha + 1;
 		}
 	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
